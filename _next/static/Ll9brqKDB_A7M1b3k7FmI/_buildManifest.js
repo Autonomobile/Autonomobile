62503a8de7718264e8cb46a663dd3c75cdbf8992
Mickael Bobovitchcd <m.bobovitch@gmail.com>
2022-03-08 16:12:27 +0100

    new version

---
+++ _next/static/Ll9brqKDB_A7M1b3k7FmI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/528-b30ece4ec7730561.js","static/chunks/pages/index-1f94ab59b2451c81.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/QIV2PfEjWmYjTm2PyFigg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/528-427a5f89203a14dd.js","static/chunks/pages/index-d6801b2d1a771c98.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
