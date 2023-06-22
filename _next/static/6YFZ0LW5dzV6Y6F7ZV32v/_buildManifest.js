52eeb718503bdf15c39f2673e318e8da4ce0f31a
Mickael Bobovitch <mickael.bobovitch@epita.fr>
2023-06-22 20:49:29 +0200

    final

---
+++ _next/static/6YFZ0LW5dzV6Y6F7ZV32v/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-6988a4c603344d86.js","static/chunks/849-acb811f9f3adbf18.js","static/chunks/pages/index-60da6e1e4277738b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
